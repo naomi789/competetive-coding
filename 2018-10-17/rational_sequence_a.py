a96ea44cd4f82cfcef2905b9aca00f7ba2f51f6b
naomi789 <snjohnson789@gmail.com>
2018-10-17 21:11:07 -0400

    saved as ints not strings

--- 2018-10-17/rational_sequence_a.py
+++ 2018-10-17/rational_sequence_a.py
@@ -6,12 +6,12 @@ def read():
     numer_denom = 0
     all_pairs = []
     for iteration in range(0, num_rows):
-        case_numer_denom = input()
+        case_numer_denom = input().strip('\r')
         case,numer_denom = case_numer_denom.split(" ")
         numer, denom = numer_denom.split("/")
-        print(case, numer, denom)
-        # numbers.append(int(input()))
+        all_pairs.append((int(case), int(numer), int(denom)))
 
+    print(all_pairs)
     return all_pairs
 
 
