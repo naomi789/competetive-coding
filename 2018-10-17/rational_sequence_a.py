bb7678d7e5531324b65b74049c0bc992a753054e
naomi789 <snjohnson789@gmail.com>
2018-10-17 21:07:29 -0400

    rational sequence, test case, reading input start

---
+++ 2018-10-17/rational_sequence_a.py
@@ -0,0 +1,20 @@
+# problem: https://open.kattis.com/contests/dt5jiu/problems/rationalsequence2
+
+def read():
+    num_rows = int(input())
+
+    numer_denom = 0
+    all_pairs = []
+    for iteration in range(0, num_rows):
+        numer_denom = input()
+        print(numer_denom)
+        # numbers.append(int(input()))
+
+    return all_pairs
+
+
+def main(all_pairs):
+    pass
+
+
+main(read())
