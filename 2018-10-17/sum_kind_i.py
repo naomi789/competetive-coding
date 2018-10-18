39b059b88cb351b8a0af24bd2dab359d1a397a41
naomi789 <snjohnson789@gmail.com>
2018-10-17 20:43:52 -0400

    too slow but solved

--- 2018-10-17/sum_kind_i.py
+++ 2018-10-17/sum_kind_i.py
@@ -3,10 +3,23 @@ def read():
     return num_numbers
 
 
-def main(data):
-    pass
+def main(num_numbers):
+    for iter in range(0, num_numbers):
+        two_nums = input().strip('\r').split(" ")
+        important_num = int(two_nums[1])
+        test_name = int(two_nums[0])
+
+        s1 = 0
+        s2 = 0
+        s3 = 0
+        for num in range(0, important_num):
+            s1 = s1 + num + 1
+            s2 = s2 + 2*num + 1
+            s3 = s3 + 2*num + 2
+
+        print(test_name, s1, s2, s3)
 
 
 data = read()
-print(data)
+# print(data)
 main(data)
