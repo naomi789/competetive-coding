efeef70c8d4816d6ac7b1882d13c52eb0e25cf00
naomi789 <snjohnson789@gmail.com>
2018-10-24 16:53:49 -0400

    read input

--- 2018-10-24/foosball.py
+++ 2018-10-24/foosball.py
@@ -1,5 +1,21 @@
+import queue
+
 data = input()
 
 names = input().strip('/r').split(" ")
 
-print(data)
+WhiteO = names[0]
+BlackO = names[1]
+WhiteD = names[2]
+BlackD = names[3]
+
+line = queue.Queue()
+
+for num in range(len(names) - 4):
+    line.put(names[num + 4])
+
+feed = input().strip('/r')
+
+for num in range(len(feed)):
+    if (feed[num] == 'W'):
+        WhiteO = WhiteD
