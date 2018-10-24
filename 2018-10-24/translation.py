c4cfb38698f04cee84f5992740d00826b4853ead
naomi789 <snjohnson789@gmail.com>
2018-10-24 16:59:33 -0400

    input

--- 2018-10-24/translation.py
+++ 2018-10-24/translation.py
@@ -1,5 +1,14 @@
 def read():
-    pass
+    print('hi')
+    first_line = input()
+    num_languages, num_translators = first_line.split(' ')
+    num_languages = int(num_languages)
+    num_translators = int(num_translators)
+    all_langs = input()
+    langs = []
+    for lang in all_langs.split(' '):
+        langs.append(lang)
+    print(langs)
 
 
 def determine(target_langs, ):
