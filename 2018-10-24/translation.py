116e0da0509b4ea92d0ce3371efe651e2452d02e
naomi789 <snjohnson789@gmail.com>
2018-10-24 17:20:32 -0400

    reads in data, makes default dict

--- 2018-10-24/translation.py
+++ 2018-10-24/translation.py
@@ -1,17 +1,31 @@
+from collections import defaultdict
 def read():
-    print('hi')
     first_line = input()
     num_languages, num_translators = first_line.split(' ')
     num_languages = int(num_languages)
     num_translators = int(num_translators)
     all_langs = input()
-    langs = []
+    goal_langs = []
+    translators = defaultdict(list)
     for lang in all_langs.split(' '):
-        langs.append(lang)
-    print(langs)
+        goal_langs.append(lang)
+    for iteration in range(0, num_translators):
+        start, end, cost = input().split(' ')
+        cost = int(cost)
+
+        translators[start].append((end, cost))
+
+        # if not start in translators.keys():
+        #     translators[start] = (end, cost)
+        # else:
+        #     translators[start] = translators[start].append((end, cost))
+
+    print(goal_langs)
+    print(translators)
+    return goal_langs, translators
 
 
-def determine(target_langs, ):
+def determine(target_langs):
     pass
 
 
@@ -19,3 +33,4 @@ def helper():
     pass
 
 
+determine(read())
