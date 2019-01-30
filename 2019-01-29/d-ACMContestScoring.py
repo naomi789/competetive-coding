25669fb283e6c5b870b3437a46e148d923ee4a20
Naomi Johnson <sajohnso@apples-mbp.sea.adobe.com>
2019-01-29 18:40:59 -0800

    input is broken

---
+++ 2019-01-29/d-ACMContestScoring.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# The input contains n lines for 0≤n≤100, with each line describing a particular log entry. 
+# A log entry has three parts: an integer m, with 1≤m≤300, designating the number of minutes 
+# at which a submission was received, an uppercase letter designating the problem, and either 
+# the word right or wrong. The integers will be in nondecreasing order and may contain 
+# repeats. After all the log entries is a line containing just the number −1.
+
+def read():
+    # text_file = open('d-input.txt', 'r')
+    text_file = input()
+    print(text_file)
